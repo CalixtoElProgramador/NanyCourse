47679485655d0f37dfc119e04d155d07ea8389d9
calixto.programador@gmail.com <calixto.programador@gmail.com>
2022-04-04 21:31:17 -0500

    Tarea 04 asiganada.

---
+++ Fundamentos_01/Tareas/Tarea04.js
@@ -0,0 +1,8 @@
+// TODO: Tarea #04: Aplicar todo lo que has visto en Fundamento I en una tienda electrónica. El tema de tu tienda puede ser cualquiera. Utiliza el módulo readline para gestionar los inputs del usuario.
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
